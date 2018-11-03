fda56591085c82e2fa055489e7200c1e9aa6a506
USAS12 <44721800+USAS12@users.noreply.github.com>
2018-11-03 17:14:05 +0900

    Calculator
    HLOLOLOOLOLOLOOOO

---
+++ Helloworld.py
@@ -0,0 +1,14 @@
+import Main
+check = int(input("1 : sum, 2 : dec, 3 : mul, 4 : div"))
+num1_ = float(input("num1 ="))
+num2_ = float(input("num2 ="))
+if check==1:
+    Main.sum(num1_,num2_)
+elif check==2:
+    Main.sum(num1_,num2_)
+elif check==3:
+    Main.mul(num1_,num2_)
+elif check==4:
+    Main.div(num1_,num2_)
+else: print("error")
+
---
+++ Main.py
@@ -0,0 +1,10 @@
+def sum(num1,num2):
+    print("num1+num2 =",float(num1)+float(num2))
+def dec(num1,num2):
+    print("num1-num2 =",float(num1)-float(num2))
+def mul(num1,num2):
+    print("num1*num2 =",float(num1)*float(num2))
+def div(num1,num2):
+    if num2 !=0:
+        print("num1/num2 =",float(num1)/float(num2))
+    else: print("num2 must be not 0")
